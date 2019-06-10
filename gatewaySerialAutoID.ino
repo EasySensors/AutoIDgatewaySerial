4e3dc5030c49be0962a0bb3e41fc6c475bdf5e94
Y <yury.svirida@gmail.com>
2019-06-10 13:34:18 +0200

    Create gatewaySerialAutoID.ino

---
+++ gatewaySerialAutoID.ino
@@ -0,0 +1,207 @@
+  /**
+ * The MySensors Arduino library handles the wireless radio link and protocol
+ * between your home built sensors/actuators and HA controller of choice.
+ * The sensors forms a self healing radio network with optional repeaters. Each
+ * repeater and gateway builds a routing tables in EEPROM which keeps track of the
+ * network topology allowing messages to be routed to nodes.
+ *
+ * Created by Henrik Ekblad <henrik.ekblad@mysensors.org>
+ * Copyright (C) 2013-2015 Sensnology AB
+ * Full contributor list: https://github.com/mysensors/Arduino/graphs/contributors
+ *
+ * Documentation: http://www.mysensors.org
+ * Support Forum: http://forum.mysensors.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * version 2 as published by the Free Software Foundation.
+ *
+ *******************************
+ *
+ * DESCRIPTION
+ * The ArduinoGateway prints data received from sensors on the serial link.
+ * The gateway accepts input on seral which will be sent out on radio network.
+ *
+ * The GW code is designed for Arduino Nano 328p / 16MHz
+ *
+ * Wire connections (OPTIONAL):
+ * - Inclusion button should be connected between digital pin 3 and GND
+ * - RX/TX/ERR leds need to be connected between +5V (anode) and digital pin 6/5/4 with resistor 270-330R in a series
+ *
+ * LEDs (OPTIONAL):
+ * - To use the feature, uncomment any of the MY_DEFAULT_xx_LED_PINs
+ * - RX (green) - blink fast on radio message recieved. In inclusion mode will blink fast only on presentation recieved
+ * - TX (yellow) - blink fast on radio message transmitted. In inclusion mode will blink slowly
+ * - ERR (red) - fast blink on error during transmission error or recieve crc error
+ *
+ */
+
+#include <EEPROM.h>
+
+// Enable debug prints to serial monitor
+#define MY_DEBUG
+//#define MY_DEBUG_VERBOSE_RFM69
+
+// Enable and select radio type attached
+
+//#define MY_RADIO_NRF24
+
+#define MY_RADIO_RFM69
+//#define MY_IS_RFM69HW
+
+#define MY_RFM69_FREQUENCY   RFM69_915MHZ
+//#define MY_RFM69_FREQUENCY   RFM69_868MHZ
+//#define MY_RFM69_FREQUENCY   RFM69_433MHZ
+//#define MY_RFM69_NEW_DRIVER  
+
+
+//#define MY_RADIO_RFM95
+#define MY_RFM95_MODEM_CONFIGRUATION RFM95_BW_500KHZ | RFM95_CODING_RATE_4_5, RFM95_SPREADING_FACTOR_2048CPS | RFM95_RX_PAYLOAD_CRC_ON, RFM95_AGC_AUTO_ON // 
+#define MY_RFM95_TX_POWER_DBM (20u)
+
+//#define   MY_RFM95_FREQUENCY RFM95_915MHZ
+#define   MY_RFM95_FREQUENCY RFM95_868MHZ
+//#define   MY_RFM95_FREQUENCY RFM95_433MHZ
+
+//#define MY_SIGNING_ATSHA204_PIN A2
+
+//#define MY_SIGNING_ATSHA204
+//#define  MY_SIGNING_REQUEST_SIGNATURES
+
+
+//#define MY_RFM95_FREQUENCY   RFM95_915MHZ
+
+//#define MY_OTA_FIRMWARE_FEATURE
+
+// Set LOW transmit power level as default, if you have an amplified NRF-module and
+// power your radio separately with a good regulator you can turn up PA level.
+#define MY_RF24_PA_LEVEL RF24_PA_LOW
+
+// Enable serial gateway
+#define MY_GATEWAY_SERIAL
+
+// Define a lower baud rate for Arduino's running on 8 MHz (Arduino Pro Mini 3.3V & SenseBender)
+#if F_CPU == 8000000L
+#define MY_BAUD_RATE 38400
+#endif
+
+// Enable inclusion mode
+#define MY_INCLUSION_MODE_FEATURE
+// Enable Inclusion mode button on gateway
+//#define MY_INCLUSION_BUTTON_FEATURE
+
+// Inverses behavior of inclusion button (if using external pullup)
+//#define MY_INCLUSION_BUTTON_EXTERNAL_PULLUP
+
+// Set inclusion mode duration (in seconds)
+#define MY_INCLUSION_MODE_DURATION 60
+// Digital pin used for inclusion mode button
+//#define MY_INCLUSION_MODE_BUTTON_PIN  3
+
+// Set blinking period
+#define MY_DEFAULT_LED_BLINK_PERIOD 300
+
+// Inverses the behavior of leds
+//#define MY_WITH_LEDS_BLINKING_INVERSE
+
+// Flash leds on rx/tx/err
+// Uncomment to override default HW configurations
+//#define MY_DEFAULT_ERR_LED_PIN 4  // Error led pin
+//#define MY_DEFAULT_RX_LED_PIN  6  // Receive led pin
+//#define MY_DEFAULT_TX_LED_PIN  5  // the PCB, on board LED
+
+#include <MySensors.h>
+
+void before() {
+}
+
+byte getFreeAddress(){
+  byte routes[256];
+  hwReadConfigBlock((void*)&routes, (void*)EEPROM_ROUTES_ADDRESS, SIZE_ROUTES);
+  for(int i=1;i<255;i++){
+      if (routes[i] == 255){
+        return i;
+      }
+  }
+  return 0;
+}
+
+void setup() {
+  // Setup locally attached sensors
+  byte routes[256];
+  hwReadConfigBlock((void*)&routes, (void*)EEPROM_ROUTES_ADDRESS, SIZE_ROUTES);
+  for(int i=0;i<255;i++){
+      Serial.print("ID "); 
+      Serial.print(i); 
+      Serial.print(" "); 
+      Serial.println(routes[i]);
+  }
+}
+
+void presentation() {
+ // Present locally attached sensors
+}
+
+boolean flag = false;
+byte reqSensorId = 0;
+
+MyMessage msg;
+
+void loop() {
+  // Send locally attached sensor data here
+  if (flag) {
+    flag = false;
+    byte newAddr = getFreeAddress();
+    if (newAddr > 0) {
+      (void)transportRouteMessage(build(msg, 255, reqSensorId, C_INTERNAL, I_ID_RESPONSE).set((uint8_t) newAddr));
+    }
+  }
+}
+
+void receive(const MyMessage &message) { 
+
+ if (message.type==I_ID_REQUEST) {
+    Serial.println("rrrrrrrrrrrrrrr");
+    flag = true;
+    reqSensorId = message.sensor; 
+ }
+}
+
+
+
+
+
+
+// some debug lines
+   //message.setDestination();//message.sender
+   //mSetCommand(message, C_INTERNAL);
+   //message.setType(I_ID_RESPONSE);
+   //send(message.set(5), false);
+   
+ /*
+    Serial.print("COMMAND: ");
+    Serial.println(mGetCommand(msg));
+   wait(100);
+    send(msg); 
+    */  
+   
+    //MyMessage msg;
+
+    //build(msg, 255, 1, C_INTERNAL, I_ID_RESPONSE).set((uint8_t)0x05);
+    
+    
+    /* 
+      Serial.begin(115200);
+  Serial.println("!");
+  pinMode(9,OUTPUT);
+  digitalWrite(9,LOW);
+  wait(10);
+  digitalWrite(9,HIGH);
+  wait(10);
+  digitalWrite(9,LOW);
+  wait(10);
+  */
+  
+    //Serial.print("mem1 ");
+  //Serial.println(sizeof(routingTable_t));
+  
